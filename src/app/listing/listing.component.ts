ac5703aa7ac438edab18ab836fbd52613e318df0
afu <ademafu@hotmail.com>
2024-12-05 08:38:01 +0100

    Minor adjustments

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -9,43 +9,35 @@ import { Car } from '../car';
   template: `
     <h1>Saved Cars {{ savedCarList.length }}</h1>
     <section class="container">
-      <!-- This article element represents and entire listing -->
       @for(carEntry of carList; track carEntry){
       <article class="listing">
         <div class="image-parent">
           <img class="product-image" src="https://placehold.co/100x100" />
         </div>
         <section class="details">
-          <p class="title">
-            <!-- car make and model-->
-            {{ carEntry.make }} - {{ carEntry.model }}
-          </p>
+          <p class="title">{{ carEntry.make }} - {{ carEntry.model }}</p>
           <hr />
           <p class="detail">
             <span>Year</span>
             <span>
-              <!-- year -->
               {{ carEntry.year }}
             </span>
           </p>
           <div class="detail">
             <span>Transmission</span>
             <span>
-              <!-- transmission -->
               {{ carEntry.transmission }}
             </span>
           </div>
           <p class="detail">
             <span>Mileage</span>
             <span>
-              <!-- miles -->
               {{ carEntry.miles }}
             </span>
           </p>
           <p class="detail">
             <span>Price</span>
             <span>
-              <!-- price -->
               {{ carEntry.price }}
             </span>
           </p>
@@ -53,12 +45,10 @@ import { Car } from '../car';
         <button (click)="addCarToSaved(carEntry)">Save car</button>
       </article>
       }
-      <!-- end car listing markup -->
     </section>
 
     <article>
       <h2>Saved Cars</h2>
-
       <ul>
         @for(car of savedCarList; track car){
         <li>{{ car.make }} - {{ car.model }}</li>
@@ -108,20 +98,3 @@ export class ListingComponent {
     this.savedCarList.push(car);
   }
 }
-
-// <article>
-//   <!-- @for(checkbox of checkboxes; track checkbox){  -->
-//   <app-checkboxes />
-//   <!-- } -->
-// </article>
-
-// <article>
-//   <h1>Exiting routing!</h1>
-//   @for(title of productTitles; track title){
-//   <p>
-//     <a [routerLink]="['details', $index]">
-//       {{ title }}
-//     </a>
-//   </p>
-//   }
-// </article>
